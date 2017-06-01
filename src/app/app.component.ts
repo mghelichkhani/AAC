da9ad1d4e7d5ec0424fe7a4d52f07bf2480bf08b
mghelichkhani <m.ghelichkhani@gmail.com>
2017-06-01 14:37:38 +0200

    FEATURE: MainMenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { SettingsPage } from '../pages/settings.page/settings.page';
 import { HomePage } from '../pages/home/home';
+
+export interface MenuItem {
+  title: string;
+  component: any;
+  icon: string;
+}
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar) {
-    platform.ready().then(() => {
-      statusBar.styleDefault();
-    });
+   rootPage: any = HomePage;
+
+   appMenuItems: Array<MenuItem>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+
+    this.initializeApp();
+    this.appMenuItems = [
+      { title: 'Home', component: HomePage, icon: 'text' },
+      { title: 'Settings', component: SettingsPage, icon: 'settings' }
+    ]
   }
+
+    initializeApp() {
+      this.platform.ready().then(() => {
+        this.statusBar.styleDefault();
+        this.splashScreen.hide();
+      });
+    }
+
+    openPage(page) {
+      // Reset the content nav to have just this page
+      // we wouldn't want the back button to show in this scenario
+      this.nav.setRoot(page.component);
+    }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DataProvider } from '../../providers/data.provider';
 import { SentenceProvider } from '../../providers/sentence.provider';
+import { OptionsProvider } from '../../providers/options.provider';
 import { SymbolGrid } from '../../components/symbol-grid';
 import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { SettingsPage } from '../settings.page/settings.page';
 
 @Component({
   selector: 'page-home',
@@ -16,12 +18,19 @@ export class HomePage {
 
   }
 
+  load() {
+    // this.navCtrl.push(SettingsPage, {
+    //   someKey: 'something'
+    // });
+  }
+
+
   setDummyData() {
-    this.dataProvider.addSymbol({
-      id: 58,
-      caption: 'ball',
-      image: 'http://placehold.it/128x128?text=ball'
-    });
+    // this.dataProvider.addSymbol({
+    //   id: 58,
+    //   caption: 'ball',
+    //   image: 'http://placehold.it/128x128?text=ball'
+    // });
   }
 
 }
