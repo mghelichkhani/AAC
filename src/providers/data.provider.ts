f0a8a171036fce543917ee6626a55e4a86025853
mghelichkhani <m.ghelichkhani@gmail.com>
2017-07-12 23:00:33 +0200

    Updates on DataProvider

--- src/providers/data.provider.ts
+++ src/providers/data.provider.ts
@@ -4,21 +4,108 @@ import 'rxjs/add/operator/map';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AlertController } from 'ionic-angular';
 
+// Symbol Interface
+export interface Symbol {
+  id: string,
+  name: string,
+  caption: string,
+  search_string: string,
+  relevance: number,
+  picture: {  
+    extension: string,
+    image_url: string
+  },
+  license: {
+    license: string,
+    license_url: string,
+    author: string,
+    author_url: string
+  },
+}
+
+// Symbol Category Interface
+export interface Category {
+  id: string,
+  name: string,
+  caption: string
+}
+
+// Symbol-Category Join
+export interface SymbolCategory {
+  category_id: string,
+  symbols: Array<string>
+}
+
 @Injectable()
 export class DataProvider {
 
-  public apiUrl : any;
-  public symbols: any = [];
+  public apiUrl : any = 'https://www.opensymbols.org/api/v1/symbols/';
+  public symbols: Array<Symbol> = [];
+  private symbolsOnFireDB : FirebaseListObservable<any[]>;
+  private gridsOnFireDB : FirebaseListObservable<any[]>;
   private symbolsDatabaseList: FirebaseListObservable<any[]>;
+  private fireItems: FirebaseListObservable<any[]>;
   
   constructor(private http: Http, private db: AngularFireDatabase, private alertCtrl: AlertController) {
-    this.apiUrl = 'http://192.168.0.102:5000/api/v1.0/';
-    this.symbols = this.getSymbols('cat');
+
+    // Firebase DB location for now
+    this.symbolsOnFireDB = this.db.list('/all-symbols');
+    
+    // One simple data fetch for now
+    this.searchSymbols('cat');
+    console.log(this.symbols);
   }
 
   getSymbols(queryString: String) {
-    this.http.get(this.apiUrl + queryString).map(res => res.json()).subscribe(data => {
-      this.symbols = data;
+    let res: Array<Symbol> = [];
+
+    this.http.get(this.apiUrl + 'search?q=' + queryString).map(response => response.json())
+    .subscribe(data => {
+      data.forEach(element => {
+        res.push({
+          id: element.id,
+          name: element.name,
+          caption: element.name,
+          search_string: element.search_string,
+          relevance: element.relevance,
+          picture: {  
+            extension: element.extension,
+            image_url: element.image_url
+          },
+          license: {
+            license: element.license,
+            license_url: element.license_url,
+            author: element.author,
+            author_url: element.author_url
+          },
+        });
+      });
+    });
+    return res;
+  }
+
+  searchSymbols(queryString: String) {
+    this.http.get(this.apiUrl + 'search?q=' + queryString).map(response => response.json())
+    .subscribe(data => {
+      data.forEach(element => {
+        this.symbols.push({
+          id: element.id,
+          name: element.name,
+          caption: element.name,
+          search_string: element.search_string,
+          relevance: element.relevance,
+          picture: {  
+            extension: element.extension,
+            image_url: element.image_url
+          },
+          license: {
+            license: element.license,
+            license_url: element.license_url,
+            author: element.author,
+            author_url: element.author_url
+          },
+        });
+      });
     });
   }
 
@@ -26,9 +113,17 @@ export class DataProvider {
     this.symbols.push(symbol);
   }
 
-  saveGridAs() {
+  getGrids() {
+    this.fireItems = this.db.list('/symbolLibraries', {
+      query: {
+        limitToFirst: 10
+      }
+    });
+  }
+
+  saveGrid() {
     let alert = this.alertCtrl.create({
-      title: 'Save Grid As',
+      title: 'Save Grid',
       inputs: [
         {
           name: 'gridName',
